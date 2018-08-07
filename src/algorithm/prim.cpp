a46ad9ec65c61d3d5eb52aa0ead350b3315f6fbd
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-08 02:56:46 +0430

    Update prim.cpp

--- src/algorithm/prim.cpp
+++ src/algorithm/prim.cpp
@@ -1,28 +1,30 @@
-#include <queue> // std::priority_queue
 #include <vector> // std::vector<bool>
-#include <functional> // std::greater
-#include "../ds/Graph.hpp" // weight_t, wadjlist, wvpair
+#include "../ds/Graph.hpp" // Weight, WAdjList, WV, MinWVPQ
 
 // @return minimum spanning tree's weight
-weight_t prim(wadjlist& wal)
+Weight prim(WAdjList& weighted_adjlist)
 {
     std::vector<bool> taken;
-    taken.assign(wal.size(), false);
-    std::priority_queue<wvpair, vector<wvpair>, std::greater<wvpair>> pq;
+    taken.assign(weighted_adjlist.size(), false);
+    MinWVPQ pq;
 
     taken[0] = true;
-    for(wvpair& adj : wal[0]) pq.push(adj);
+    for(WV& adj_wv : weighted_adjlist[0]) pq.push(adj_wv);
 
-    weight_t mst = 0;
+    Weight mst = 0;
     while(!pq.empty())
     {
-        wvpair wvp = pq.top();
+        WV top_wv = pq.top();
         pq.pop();
 
-        if(taken[wvp.second] == true) continue;
-        taken[wvp.second] = true;
-        mst += wvp.first;
-        for(wvpair& adj : wal[wvp.second]) if(!taken[adj.second]) pq.push(adj);
+        if(taken[top_wv.second] == true) continue;
+
+        taken[top_wv.second] = true;
+        mst += top_wv.first;
+        
+        for(WV& adj_wv : weighted_adjlist[top_wv.second])
+            if(!taken[adj_wv.second])
+                pq.push(adj_wv);
     }
     
     return mst;
