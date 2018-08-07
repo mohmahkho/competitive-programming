a3494ce135d03a3c568801cb6b210962932f188c
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-08 02:33:45 +0430

    Update kruskal.cpp

--- src/algorithm/kruskal.cpp
+++ src/algorithm/kruskal.cpp
@@ -1,25 +1,25 @@
 #include <algorithm> // std::sort
-#include "../ds/Graph.hpp" // weight_t, wedgelist, wedge
-#include "../ds/UnionFind.hpp" // UnionFind
+#include <../Graph.hpp> // Weight, WEdgeList, WEdge, Vertex
+#include <../UnionFind.hpp> // UnionFind
 
 // @return minimum spanning tree's weight
-weight_t kruskal(int number_of_vertices, wedgelist& wel)
+Weight kruskal(int number_of_vertices, WEdgeList& weighted_edge_list)
 {
-    std::sort(wel.begin(), wel.end());
+    std::sort(weighted_edge_list.begin(), weighted_edge_list.end());
     UnionFind disjoint_set(number_of_vertices); // 0 based
     
     int number_of_edges = number_of_vertices - 1;
-    weight_t mst = 0;
+    Weight mst = 0;
     
-    for(wedge& we : wel) {
+    for(WEdge& weighted_edge : weighted_edge_list) {
         if(number_of_edges <= 0) break;
 
-        vertex_t u = we.second.first, v = we.second.second;
+        Vertex u = weighted_edge.second.first, v = weighted_edge.second.second;
         if(disjoint_set.in_same_set(u, v)) continue; // loop : NOT OK
 
         // no loop : OK
         disjoint_set.union_set(u, v);
-        mst += we.first;
+        mst += weighted_edge.first;
         number_of_edges--;
     }
 
