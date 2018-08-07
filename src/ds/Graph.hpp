c0f6f7bb3e7baf37d91b1f2424453758fcd9b260
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-08 02:44:42 +0430

    Update Graph.hpp

--- src/ds/Graph.hpp
+++ src/ds/Graph.hpp
@@ -3,19 +3,27 @@
 
 #include <vector> // std::vector
 #include <utility> // std::pair
+#include <queue> // std::priority_queue
+#include <functional> // std::greater
 
 // All containers can be used in directed or undirected graphs
-typedef int vertex_t; // change when needed
-typedef int weight_t; // change when needed
+typedef int Vertex; // change when needed
+typedef int Weight; // change when needed
 
-typedef std::pair<weight_t, vertex_t> wvpair;
-typedef std::pair<vertex_t, vertex_t> edge; 
-typedef std::pair<weight_t, std::pair<vertex_t, vertex_t>> wedge; 
+typedef std::pair<Weight, Vertex> WV;
+typedef std::pair<Vertex, Vertex> Edge; 
+typedef std::pair<Weight, Edge> WEdge;
 
-typedef std::vector<std::vector<long>> adjmatrix; // adjmatrix mat(n, vector<long>(n));
-typedef std::vector<std::vector<vertex_t>> adjlist;
-typedef std::vector<std::vector<wvpair>> wadjlist;
-typedef std::vector<edge> edgelist;
-typedef std::vector<wedge> wedgelist;
+typedef std::vector<std::vector<long>> AdjMatrix;
+// AdjMatrix adjmat(n, vector<long>(n));
+typedef std::vector<std::vector<Vertex>> AdjList;
+typedef std::vector<std::vector<WV>> WAdjList;
+typedef std::vector<Edge> EdgeList;
+typedef std::vector<WEdge> WEdgeList;
+
+typedef std::priority_queue<WV>
+MaxWVPQ; // Maximum has a higher priority
+typedef std::priority_queue<WV, std::vector<WV>, std::greater<WV>>
+MinWVPQ; // Minimum has a higher priority
 
 #endif
