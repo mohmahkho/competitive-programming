fd23f1d1a5550a6a0c0bb20913beb42d52adf7d8
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-08 15:59:14 +0430

    Create dijkstra.cpp

---
+++ src/algorithm/dijkstra.cpp
@@ -0,0 +1,38 @@
+#include <vector> // std::vector
+#include <limits> // std::numeric_limits
+#include <../ds/Graph.hpp> // Vertex, WAdjList, Weight, MinWVPQ, WV
+
+// Fills the shortest_path vector with respect to source
+void dijkstra(Vertex source, WAdjList& adjlist, std::vector<Weight>& shortest_path)
+{
+    shortest_path.resize(adjlist.size());
+    
+    const Weight INF = std::numeric_limits<Weight>::max() / 4; // safe !overflow
+    for(Weight& weight : shortest_path) weight = INF;
+
+    shortest_path[source] = 0;
+    
+    MinWVPQ pq;
+    pq.push({0, source});
+
+    while(!pq.empty())
+    {
+        WV top_wv = pq.top(); pq.pop();
+        Weight top_weight = top_wv.first;
+        Vertex top_vertex = top_wv.second;
+
+        if(top_weight > shortest_path[top_vertex]) continue; // lazy deletion
+
+        for(WV& adj_wv : adjlist[top_vertex]) {
+            Weight adj_weight = adj_wv.first;
+            Vertex adj_vertex = adj_wv.second;
+
+            if(shortest_path[adj_vertex] <= top_weight + adj_weight)
+                continue; // does not makes the current path shorter
+            
+            // a shorter path found
+            shortest_path[adj_vertex] = top_weight + adj_weight;
+            pq.push({shortest_path[adj_weight], adj_vertex});
+        }
+    }
+}
