4f7b0397d6e1bab9efeecbdd19419139d80ba605
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-18 17:44:14 +0430

    Update range.hpp

--- src/ds/range.hpp
+++ src/ds/range.hpp
@@ -6,41 +6,43 @@ namespace mk { namespace ds {
     template < class T, class Alloc = std::allocator<T> >
     class range : public std::vector<T, Alloc>
     {
+        typedef std::vector<T, Alloc> base;
         typedef int range_type;
-        typedef typename std::vector<T>::size_type size_type;
+        typedef typename base::size_type size_type;
+        
         range_type _first; // last = first + size() // [first, last)
     public:
         range()
-        : std::vector<T>(), _first(0)
+        : base(), _first(0)
         {
         }
         
         range(range_type first, range_type last)
-        : std::vector<T>(last - first), _first(first)
+        : base(last - first), _first(first)
         {
         }
 
         range(size_type n)
-        : std::vector<T>(n), _first(0)
+        : base(n), _first(0)
         {
         }
 
         range(std::initializer_list<T> il, range_type first = 0)
-        : std::vector<T>(il), _first(first)
+        : base(il), _first(first)
         {
         }
 
         T& operator[] (range_type i) {
-            return std::vector<T>::operator[](i - _first);
+            return base::operator[](i - _first);
         }
 
         T& at (range_type i) {
-            return std::vector<T>::at(i - _first);
+            return base::at(i - _first);
         }
 
         void rearrange(range_type first, range_type last) {
             this->_first = first;
-            std::vector<T>::resize(last - first);
+            base::resize(last - first);
         }
 
         void set_first(range_type first) {
@@ -48,6 +50,6 @@ namespace mk { namespace ds {
         }
 
         range_type first() {return _first;}
-        range_type last()  {return _first + (range_type)std::vector<T>::size();}
+        range_type last()  {return _first + (range_type)base::size();}
     };
 } }
