30982ad5c1cadc8d0426348699e8b89b03beed4f
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-08-22 22:55:37 +0430

    Create competitive_template.cpp

---
+++ src/competitive_template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, a, comp, b) for(auto i = a; i comp b; ++i)
+#define per(i, a, comp, b) for(auto i = a; i comp b; --i)
+#define loop(i, a, comp, b, op) for(auto i = a; i comp b; i op)
+#define sz(x) ((int)sizeof(x))
+#define MAXIN 1000010
+#define MODER 1000000007
+#define INF 2000000000
+#define pb push_back
+#define eb emplace_back
+using ll = long long;
+using ull = unsigned long long;
+using ii = std::pair<int, int>;
+using namespace std;
+
+int main()
+{
+}
