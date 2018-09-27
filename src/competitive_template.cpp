15be69a1a890a5ddeca2f90ae5b34014173e32ee
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-09-27 21:53:36 +0330

    Update competitive_template.cpp

--- src/competitive_template.cpp
+++ src/competitive_template.cpp
@@ -1,18 +1,22 @@
 #include <bits/stdc++.h>
-#define rep(i, a, comp, b) for(auto i = a; i comp b; ++i)
-#define per(i, a, comp, b) for(auto i = a; i comp b; --i)
+#define rep(i, a, comp, b)      for(auto i = a; i comp b; ++i)
+#define per(i, a, comp, b)      for(auto i = a; i comp b; --i)
 #define loop(i, a, comp, b, op) for(auto i = a; i comp b; i op)
+#define repeat(t)               for(auto tmp__ = t; tmp__ > 0; --tmp__)
 #define sz(x) ((int)sizeof(x))
-#define MAXIN 100010
-#define MOD 1000000007
-#define INF 2000000000
 #define pb push_back
 #define eb emplace_back
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MAXINPUT = 1e5 + 10;
 using ll  = long long;
 using ull = unsigned long long;
 using ii  = std::pair<int, int>;
+template <class T>
+using matrix = std::vector<std::vector<T>>;
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 }
