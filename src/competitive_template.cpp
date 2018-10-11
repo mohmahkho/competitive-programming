a69268879c82dde850f135cdb0986ceec0cfb60d
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-10-11 22:23:18 +0330

    Update competitive_template.cpp

--- src/competitive_template.cpp
+++ src/competitive_template.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
-#define rep(i, a, comp, b)      for(auto i = a; i comp b; ++i)
-#define per(i, a, comp, b)      for(auto i = a; i comp b; --i)
-#define loop(i, a, comp, b, op) for(auto i = a; i comp b; i op)
-#define repeat(t)               for(auto tmp__ = t; tmp__ > 0; --tmp__)
+// -----------------------------------------------------------------------------
+#define REP(i, a, comp, b)      for(auto i = a; i comp b; ++i)
+#define PER(i, a, comp, b)      for(auto i = a; i comp b; --i)
+#define LOOP(i, a, comp, b, op) for(auto i = a; i comp b; i op)
+#define REPEAT(t)               for(auto HDN_CTR = t; HDN_CTR > 0; --HDN_CTR)
+#define CFOR(i, a, b)           for(auto i = a; i <= b; ++i)
+#define OFOR(i, a, b)           for(auto i = a; i < b; ++i)
 #define sz(x) ((int)sizeof(x))
 #define pb push_back
 #define eb emplace_back
+#define endl '\n'
+// -----------------------------------------------------------------------------
+using LL  = long long;
+using ULL = unsigned long long;
+using IIP = std::pair<int, int>;
+using LLP = std::pair<long long, long long>;
+template <class T>
+using Matrix = std::vector<std::vector<T>>;
+template <class UIntType>
+using AdjList = std::vector<std::vector<UIntType>>;
+// -----------------------------------------------------------------------------
 constexpr int INF = 2e9;
 constexpr int MOD = 1e9 + 7;
 constexpr int MAXINPUT = 1e5 + 10;
-using ll  = long long;
-using ull = unsigned long long;
-using ii  = std::pair<int, int>;
-template <class T>
-using matrix = std::vector<std::vector<T>>;
+// -----------------------------------------------------------------------------
 using namespace std;
 
-int main()
-{
+void solve_() {
+
+}
+
+int main() {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    solve_();
+
 }
