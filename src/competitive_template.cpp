95e2979ee7002147b5f932d4b1688aec95018214
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-10-13 22:12:08 +0330

    Update competitive_template.cpp

--- src/competitive_template.cpp
+++ src/competitive_template.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 // -----------------------------------------------------------------------------
-#define REP(i, a, comp, b)      for(auto i = a; i comp b; ++i)
-#define PER(i, a, comp, b)      for(auto i = a; i comp b; --i)
-#define LOOP(i, a, comp, b, op) for(auto i = a; i comp b; i op)
-#define REPEAT(t)               for(auto HDN_CTR = t; HDN_CTR > 0; --HDN_CTR)
-#define CFOR(i, a, b)           for(auto i = a; i <= b; ++i)
-#define OFOR(i, a, b)           for(auto i = a; i < b; ++i)
+#define rep(i, a, comp, b)      for(auto i = a; i comp b; ++i)
+#define per(i, a, comp, b)      for(auto i = a; i comp b; --i)
+#define loop(i, a, comp, b, op) for(auto i = a; i comp b; i op)
+#define repeat(t)               for(auto HDN_CTR = t; HDN_CTR > 0; --HDN_CTR)
+// c : closed // o : open // rof : backwards
+#define cfor(i, a, b)           for(auto i = a; i <= b; ++i)
+#define ofor(i, a, b)           for(auto i = a; i < b; ++i)
+#define crof(i, b, a)           for(auto i = b; i >= a; --i)
+#define orof(i, b, a)           for(auto i = b; i > a; --i)
 #define sz(x) ((int)sizeof(x))
 #define pb push_back
 #define eb emplace_back
@@ -15,23 +18,26 @@ using LL  = long long;
 using ULL = unsigned long long;
 using IIP = std::pair<int, int>;
 using LLP = std::pair<long long, long long>;
+using VL  = std::vector<long long>;
+using VI  = std::vector<int>;
+using VVI = std::vector<std::vector<int>>;
 template <class T>
-using Matrix = std::vector<std::vector<T>>;
-template <class UIntType>
-using AdjList = std::vector<std::vector<UIntType>>;
+using Vec2D = std::vector<std::vector<T>>;
+template <class T>
+using AdjList = Vec2D<T>; // just an alias to be more readable!
 // -----------------------------------------------------------------------------
 constexpr int INF = 2e9;
+constexpr long long LLINF = 4523372036854775807LL;
 constexpr int MOD = 1e9 + 7;
 constexpr int MAXINPUT = 1e5 + 10;
 // -----------------------------------------------------------------------------
 using namespace std;
 
-void solve_() {
-
+void SOLVE_PROBLEM() {
+    
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    solve_();
-
+    std::ios::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0);
+    SOLVE_PROBLEM();
 }
