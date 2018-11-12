282178680fb14a80f7e1ace3c158d69c420bc332
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-11-12 23:30:43 +0330

    Update least_prime_factor.cpp

--- src/algorithm/least_prime_factor.cpp
+++ src/algorithm/least_prime_factor.cpp
@@ -2,7 +2,7 @@
 template <class Integer>
 Integer least_prime_factor(Integer n) {
     // returns smallest prime factor of n
-    for(Integer i = 2; i * i <= n; i++) {
+    for(Integer i = 2; i * i <= n; ++i) {
         if(n % i == 0) return least_prime_factor(i);
     }
 
