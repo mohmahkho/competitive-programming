933ee7bcab72e58376920482b60ec1ce9b7086d1
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2018-12-27 00:16:35 +0330

    Update common_mistakes.cpp

--- src/common_mistakes.cpp
+++ src/common_mistakes.cpp
@@ -15,3 +15,31 @@ an underflow.
 for(int i = 0; i < (int)vec.size() - 2; i++) {
     std::cout << vec[i] << std::endl;
 }
+
+// =======================================================================
+/*
+once I was trying to fide vertex with maximul degree in a graph and
+I wrote the code below:
+*/
+int max_deg = -1;
+int max_deg_vertex;
+for(int v = 1; v <= n; v++) {
+  if(adjlist[v].size() > max_deg) {
+    max_deg = adjlist[v].size();
+    max_deg_vertex = v;
+  }
+}
+/*
+this super simple code intrestingly does not work. The reason may not be 
+obvios at first glace but the thing that happening here is that
+comparison between unsigned_integer and signed_integer caused the signed one
+to be casted to unsigned so that the value of that would be maximum value of
+unsigned integer. this mistake is basikly the previos one.
+*/
+// Solution 1:
+int max_deg = -1;
+int(adjlist[v].size()) > max_deg     
+// Solution 2:
+int max_deg = 0;
+adjlist[v].size() > max_deg
+
