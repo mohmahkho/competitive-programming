6b1ce20b9219fb61f32d4c0946ddee4228701b86
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-01-17 20:41:41 +0330

    Update least_prime_factor.cpp

--- src/math/least_prime_factor.cpp
+++ src/math/least_prime_factor.cpp
@@ -1,8 +1,6 @@
-// Integer Type : int, long, long long, unsigned int, ...
-template <class Integer>
-Integer least_prime_factor(Integer n) {
+int least_prime_factor(int n) {
     // returns smallest prime number that devides n
-    for(Integer i = 2; i * i <= n; ++i) {
+    for(int i = 2; i * i <= n; ++i) {
         if(n % i == 0) return least_prime_factor(i);
     }
 
