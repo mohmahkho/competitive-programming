ad7ea4843e4c909e1e9dc694dc68db60637643c8
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-01-20 22:06:36 +0330

    Update cntrem.cpp

--- src/math/cntrem.cpp
+++ src/math/cntrem.cpp
@@ -5,7 +5,7 @@ inline int cntrem(int n, int m, int r) {
 } // O(1)
 
 /* Logic :
-    devide numbers in number line into groups of remainders for example:
+    devide numbers in number line into groups of remainders for example for 3:
     {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, ...
     if we devide any number in any group by m, we get (because this is an integer division)
     we'll get the number of blocks before this block.
