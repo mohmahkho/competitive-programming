b5a56f3313469c1ae5ddefa378e17ab44b3f5ce5
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-02-10 22:00:21 +0330

    Update least_prime_factor.cpp

--- src/math/least_prime_factor.cpp
+++ src/math/least_prime_factor.cpp
@@ -1,7 +1,7 @@
 int least_prime_factor(int n) {
     // returns smallest prime number that devides n
     for(int i = 2; i * i <= n; ++i) {
-        if(n % i == 0) return least_prime_factor(i);
+        if(n % i == 0) return i; // i is a prime factor of n
     }
 
     // n is prime
