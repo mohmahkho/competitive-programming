32118e63c0147bb83acdb7bb3a147676744b8d75
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-02-26 20:27:00 +0330

    Update 0-1 BFS.cpp

--- src/algorithm/Graphs/0-1 BFS.cpp
+++ src/algorithm/Graphs/0-1 BFS.cpp
@@ -21,3 +21,12 @@ while(!dq.empty())
         else dq.push_front(v);
     }
 }
+
+/*
+Dial's algorithm
+We can extend this even further if we allow the weights of the edges to be even bigger.
+If every edge in the graph has a weight ≤k, than the distances of vertices in the queue will differ by at most k 
+from the distance of v to the source. So we can keep k+1 buckets for the vertices in the queue, and whenever the bucket 
+corresponding to the smallest distance gets empty, we make a cyclic shift to get the bucket with the next higher distance.
+This extension is called Dial's algorithm.
+*/
