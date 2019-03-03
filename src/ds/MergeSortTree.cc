fb9dde89d4e72c95072252daba26d879c1727207
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-03-03 14:24:31 +0330

    Update MergeSortTree.cc

--- src/ds/MergeSortTree.cc
+++ src/ds/MergeSortTree.cc
@@ -61,3 +61,26 @@ public:
         return hmlt(x, l, r, 1, idx_begin, idx_end);
     }
 };
+
+/*
+when using merge sort we devide array into two pieces and sort them then merge them up
+so we can create a tree that the first node of it is the whole array but sorted
+left child is first half of the array but sorted and right child is second half of the array but sorted
+
+ for example if we have array 3, 2, 1, 3, 5, 1, 5, 4
+ 
+ then the tree would be like :
+                                1, 1, 2, 3, 3, 4, 5, 5
+                                    /          \
+                                   /            \
+                                  /              \
+                             1, 2, 3, 3      1, 4, 5, 5
+                              ...               ...
+                              
+
+we can use this structure to answer the queries like : "how many numbers are less than 3 in the range of 3, 8"
+in O(lg(n)*lg(n))
+
+the space complexity of this data structure is O(n * lg(n))
+because merge sort tree has lg(n) layers and at each layer we have the whole array. 
+*/
