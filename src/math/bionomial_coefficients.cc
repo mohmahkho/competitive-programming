a72dd38a800c58608766076d01ea5c5cfeac5ec4
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-03-17 17:00:22 +0330

    Update bionomial_coefficients.cc

--- src/math/bionomial_coefficients.cc
+++ src/math/bionomial_coefficients.cc
@@ -1,8 +1,7 @@
-
 int const N = 1009, MOD = 998244353;
 int bc[N][N];
 
-inline int bio(int n, int k) {
+int bio(int n, int k) {
     if(k > n) return 0;
     if(bc[n][d] != 0) return bc[n][k];
     else return bc[n][k] = (bio(n - 1, k) + bio(n - 1, k - 1)) % MOD;
