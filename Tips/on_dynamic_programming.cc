7597811a5ae5cf48e0e2baca3da6c4d1c0ac70e5
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-03-17 16:54:31 +0330

    Create on_dynamic_programming.cc

---
+++ Tips/on_dynamic_programming.cc
@@ -0,0 +1,8 @@
+/*
+one of the most important things in solving dynamic programming problems is 
+defining dp well. for example :
+dp[i] = number of good sequences ending at i or before i
+these kind of definitions help you better undrestand what you are doing and
+better set the base cases which is really important in dynamic programming problems
+because if you don't set base cases you will end up with only 0s.
+*/
