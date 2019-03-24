94f257d8cbf9a31e8c4bb27ecc24c969c2c3eab5
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-03-24 16:18:03 +0430

    Create Range Update Point Query.cc

---
+++ src/ds/Fenwick Tree/Range Update Point Query.cc
@@ -0,0 +1,37 @@
+class FenwickTree {
+    size_t n;
+    vector<int> tree;
+
+    void add(int at, int by) {
+        while(at <= n) {
+            tree[at] += by;
+            at += (at & -at);
+        }
+    }
+
+    int sum(int to) {
+        int ans = 0;
+        while(to > 0) {
+            ans += tree[to];
+            to -= (to & -to);
+        }
+        return ans;
+    }
+
+
+public:
+    FenwickTree(const vector<int>& v) { // 1 based vector
+        n = v.size() - 1;
+        tree.assign(v.size() + 1, 0);
+        for(int i = 1; i <= n; ++i) update(i, i, v[i]);
+    }
+
+    void update(int l, int r, int val) {
+        add(l, val);
+        add(r + 1, -val);
+    }
+
+    int get(int i) {
+        return sum(i);
+    }
+};
