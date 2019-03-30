513bac4452778ee7e18cd9fcd67a562d493cd467
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-03-30 15:42:53 +0430

    Update modpow.cpp

--- src/math/modpow.cpp
+++ src/math/modpow.cpp
@@ -1,12 +1,12 @@
 const int MOD = 1000 * 1000 * 1000 + 7;
 
-int mod(int n) {
-    return (n % MOD + MOD) % MOD;
+int mod(long long x) {
+    return (x % MOD + MOD) % MOD;
 }
 
 int modpow(int a, int b) {
     if (b == 0) return 1;
     int t = modpow(a, b / 2);
-    if (b % 2 == 0) return mod(t * t);
-    else return mod(mod(t * t) * a);
+    if (b % 2 == 0) return mod(1LL * t * t);
+    else return mod(1LL * mod(1LL * t * t) * a);
 }
