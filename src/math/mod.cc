5cbaffe5109755c92d912a01e27fe75c197bee0d
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-04-27 17:42:08 +0430

    Update mod.cc

--- src/math/mod.cc
+++ src/math/mod.cc
@@ -1,6 +1,7 @@
-int const MOD = 1e9 + 7;
-inline int mod(int x) {
-    return ((x % MOD) + MOD) % MOD;
+int const MOD = 1000 * 1000 * 1000 + 7;
+int mod(int x) {
+    x %= MOD;
+    return x < 0 ? x + MOD : x;
 }
 
 // this mod function works for negative numbers as well
