0fd030b26de53592928eeeea866f4e441e335088
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-05-08 11:48:34 +0430

    Create IO.cxx

---
+++ mistakes/IO.cxx
@@ -0,0 +1,8 @@
+/*
+Reading input usually is not a problem, but sometimes we need to be careful.
+for example assume that we are going to read a grid from input and we need to take
+n and m which are number of rows and colums of grid respectively.
+usually the input first contains number of rows then number of columns, but in some rare
+cases the input streams first contains number of columns then number of rows. this can be very hard
+to debug because you don't see a bug! so read the problem statement carefully!
+*/
