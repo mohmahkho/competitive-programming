69c54b243eaabfde4a3ead1672e4eb1bb20523f2
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-06-08 01:30:39 +0430

    Create Catalan.cc

---
+++ src/algorithm/Catalan.cc
@@ -0,0 +1,18 @@
+/*
+the code below generates all well-formed parentheses.
+the fact that all well-formed parentheses of lenght 2n equals the Cn
+where Cn is the n'th catalan number makes this DP approach O(Cn) time.
+*/
+
+int constexpr N = 100;
+vector<string> dp[N][N];
+
+vector<string> parentheses(int r, int c) {
+    if(r > c || r < 0 || c < 0) return {};
+    if(r == 0 && c == 0) return {""};
+    if(dp[r][c].size() != 0) return dp[r][c];
+    vector<string>& ans = dp[r][c];
+    for(string& s : parentheses(r - 1, c)) ans.push_back(s + ')');
+    for(string& s : parentheses(r, c - 1)) ans.push_back(s + '(');
+    return ans;
+}
