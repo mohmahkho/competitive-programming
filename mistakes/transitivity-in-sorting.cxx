1c0e8557134249a918ef8a998dc614aea03cf544
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-08-09 16:18:06 +0430

    Create transitivity-in-sorting.cxx

---
+++ mistakes/transitivity-in-sorting.cxx
@@ -0,0 +1,5 @@
+/*
+If you want to sort a collection of objects, and you have a comparator for it, you need to make sure
+your comparator satisfies trasitivity which is a < b AND b < c IMPLIES a < c. 
+Otherwise it will not be sortable with this comparator.
+*/
