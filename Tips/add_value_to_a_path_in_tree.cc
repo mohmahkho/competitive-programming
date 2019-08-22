3f64bd98c9e01450c07c8b60518a9a73fc3cea4f
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-08-22 15:18:51 +0430

    Update add_value_to_a_path_in_tree.cc

--- Tips/add_value_to_a_path_in_tree.cc
+++ Tips/add_value_to_a_path_in_tree.cc
@@ -1,5 +1,5 @@
 /*
-Suppose we want to add +x to the simple path from u to v in a tree.
+Suppose we want to add +x to each node in the simple path from u to v in a tree (including u and v).
 First we need to make tree rooted (possibly at 1).
 Then we should add +x to each of u and v and -2x to the lca(u, v).
 After that the answer (value of each node) will be computed by a single dfs.
