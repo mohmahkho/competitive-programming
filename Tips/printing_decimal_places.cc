b30e49da4cb34ac3fcb6d5a20eba9ab807590066
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-08-27 23:38:52 +0430

    Create printing_decimal_places.cc

---
+++ Tips/printing_decimal_places.cc
@@ -0,0 +1,5 @@
+/*
+This one may sound trivial but if your are printing a lot of real numbers with decimal places
+if you lower the precision of the output in decimal places, the amount of total time of execution of your 
+program would decrease slightly. This tip maybe useful in tight time limits.
+*/
