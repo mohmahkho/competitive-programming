974b1b1126f495703b6a5e5438a72dbdbfa12880
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-09-22 19:47:55 +0330

    Create Simpsons-Paradox.cc

---
+++ Tutor/Simpsons-Paradox.cc
@@ -0,0 +1,7 @@
+/*
+Example. 2 out of 9 women and 2 out of 10 men recieved A in the morning class.
+         6 out of 9 women and 9 out of 14 men recieved A in the afternoon class.
+         a higher proportion of women recieved A in both classes but a lower proportion of women recieved A if classes were combined!
+         The explanation is that the bulk of men are in afternoon class where it is easier to recieve A.
+         This make it easier on average for men to recieve A.
+*/
