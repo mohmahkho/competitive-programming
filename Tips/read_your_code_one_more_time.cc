0e90c8bd5fa365d84c78c9754431faaa3d249cdc
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-09-29 10:06:45 +0330

    Create read_your_code_one_more_time.cc

---
+++ Tips/read_your_code_one_more_time.cc
@@ -0,0 +1,4 @@
+/*
+Before even compiling your code, just read it again. Check if you have implemented you logic the way you should.
+check for compile errors and also logical errors. SAVE TIME!
+*/
