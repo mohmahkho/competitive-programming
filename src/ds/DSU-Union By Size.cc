41af04499be290dfbe72a8496f143af4d661b9bf
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-10-28 17:06:54 +0330

    Update DSU-Union By Size.cc

--- src/ds/DSU-Union By Size.cc
+++ src/ds/DSU-Union By Size.cc
@@ -1,16 +1,15 @@
 class DSU {
-    int n;
     vector<int> p;
 public:
-    DSU(int n) : n(n) {
-        p.assign(n, -1);
-    }   
+    DSU(int n) { p.assign(n, -1); }
     int S(int v) { return p[v] < 0 ? v : p[v] = S(p[v]); }
     int U(int u, int v) {
         u = S(u), v = S(v);
-        if(v == u) return 0;
-        if(p[u] < p[v]) swap(u, v); 
-        p[u] = v;
-        return 1;
-    }   
+        if(u != v) {
+            if(p[u] < p[v]) swap(u, v);
+            p[u] = v;
+            return 1;
+        }
+        return 0;
+    }
 };
