0d51f62923188592f2267cbf739cea47f79937b2
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-11-01 02:53:25 +0330

    Update sqrt_precision.cxx

--- mistakes/sqrt_precision.cxx
+++ mistakes/sqrt_precision.cxx
@@ -1,4 +1,5 @@
 /*
 sqrt(999999999999999999LL) in C++ is 1000000000 which is wrong.
 You can test +1 or -1 of sqrt to make sure.
+Or you may use sqrtl instead which is more precise than sqrt because it takes long double as parameter.
 */
