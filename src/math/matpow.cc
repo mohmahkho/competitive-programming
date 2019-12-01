6113334ffd58b5400ac4fd031b4a88f7715b1770
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2019-12-01 20:11:36 +0330

    Update matpow.cc

--- src/math/matpow.cc
+++ src/math/matpow.cc
@@ -1,36 +1,30 @@
-#define Matrix(n, m) vector<vector<int>>(n, vector<int>(m))
-using Matrix = vector<vector<int>>;
-int constexpr MOD = 1000 * 1000 * 1000 + 7;
-
-int mod(int x) {
-    x %= MOD;
-    return x < 0 ? x + MOD : x;
-}
-
-Matrix mul(Matrix& a, Matrix& b, int n) {
-    Matrix c = Matrix(n + 1, n + 1);
-    for(int i = 1; i <= n; ++i)
-        for(int j = 1; j <= n; ++j)
-            for(int k = 1; k <= n; ++k) {
-                c[i][j] += mod(a[i][k] * b[k][j]);
-                c[i][j] = mod(c[i][j]);
-            }
-    return c;
-}
-
-Matrix matpow(Matrix base, int n, int exp) {
-    // m is a n * n matrix
-    Matrix res = Matrix(n + 1, n + 1);
-    for(int i = 1; i <= n; ++i) res[i][i] = 1;
-    while(exp > 0) {
-        if(exp & 1) res = mul(res, base, n);
-        base = mul(base, base, n);
-        exp >>= 1;
+constexpr int M = 1000000000 + 7;
+ 
+vector<vector<int>> matmul(vector<vector<int>>& m1, vector<vector<int>>& m2, int n) {
+  vector<vector<int>> res(n, vector<int>(n, 0));
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < n; ++j) {
+      for(int k = 0; k < n; ++k) {
+        res[i][j] += 1ll * m1[i][k] * m2[k][j] % M;
+        if(res[i][j] >= M) res[i][j] -= M;
+      }
     }
-    return res;
+  }
+  return res;
+}
+ 
+vector<vector<int>> matpow(vector<vector<int>> m, int b, int n) {
+  vector<vector<int>> res(n, vector<int>(n));
+  for(int i = 0; i < n; ++i) res[i][i] = 1;
+  
+  do {
+    if(b & 1) res = matmul(res, m, n);
+    m = matmul(m, m, n);
+  } while(b >>= 1);
+  return res;
 }
 
 /*
-matpow takes a 1-based n * n matrix and return the expth power matrix of that
-in modula MOD with time complexity of O(n^3 * log(exp))
+matpow takes a 0-based n * n matrix and return the b'th power of that modulo M.
+O(n^3 * log(b))
 */
