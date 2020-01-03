e23eea61bcb2aa727d8fb3ce9e71364b6c971ac2
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-01-03 19:48:51 +0330

    Create segments_union_length.cc

---
+++ src/algorithm/segments_union_length.cc
@@ -0,0 +1,16 @@
+template<typename T>
+T segulen(const vector<pair<T, T>>& a) {
+  int n = (int) a.size();
+  vector<pair<T, int>> ends(n << 1);
+  for(int i = 0; i < n; ++i) {
+    ends[i << 1] = {a[i].first, -1};
+    ends[i << 1 | 1] = {a[i].second, 1};
+  }
+  sort(ends.begin(), ends.end());
+  T ulen = 0;
+  for(int i = 0, c = 0; i < (n << 1); ++i) {
+    ulen += (c > 0) * (ends[i].first - ends[i - 1].first);
+    c -= ends[i].second; 
+  }
+  return ulen;
+}
