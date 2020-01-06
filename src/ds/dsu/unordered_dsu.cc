5938878d406bceed425564b0c9578ce902df1852
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-01-06 17:28:31 +0330

    Update unordered_dsu.cc

--- src/ds/dsu/unordered_dsu.cc
+++ src/ds/dsu/unordered_dsu.cc
@@ -7,15 +7,15 @@ struct unordered_dsu {
     }
     return p[v] < 0 ? v : p[v] = find(p[v]);
   }
-  int unite(T u, T v) {
+  bool unite(T u, T v) {
     u = find(u);
     v = find(v);
     if(u != v) {
       if(p[u] < p[v]) swap(u, v);
       p[v] += p[u];
       p[u] = v;
-      return 1;
+      return true;
     }
-    return 0;
+    return false;
   }
 };
