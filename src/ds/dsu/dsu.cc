a99dc91630303084348f2f92746c44cc2a20bbd9
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-01-06 17:29:20 +0330

    Update dsu.cc

--- src/ds/dsu/dsu.cc
+++ src/ds/dsu/dsu.cc
@@ -6,15 +6,15 @@ struct dsu {
   int find(int v) {
     return p[v] < 0 ? v : p[v] = find(p[v]);
   }
-  int unite(int u, int v) {
+  bool unite(int u, int v) {
     u = find(u);
     v = find(v);
     if(u != v) {
       if(p[u] < p[v]) swap(u, v);
       p[v] += p[u];
       p[u] = v;
-      return 1;
+      return true;
     }
-    return 0;
+    return false;
   }
 };
