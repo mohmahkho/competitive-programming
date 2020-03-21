80465c1a926c03d62387091e06193de9c9dd429d
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-03-21 13:41:47 +0430

    Update Hash.cc

--- src/strings/Hash.cc
+++ src/strings/Hash.cc
@@ -1,29 +1,26 @@
-int mpow(int a, int b, int m) {
-  int res = 1;
-  do {
-    if(b & 1) res = 1ll * res * a % m;
-    a = 1ll * a * a % m;
-  } while(b >>= 1);
-  return res;
-}
- 
 class Hash {
-  string & s;
-  int n, B, M;
+  const int B, M;
   vector<int> bi, h;
-public:
-  Hash(string & _s, int _B, int _M) : s(_s), B(_B), M(_M) {
-    n = (int) s.size();
-    bi.resize(n);
-    int b = mpow(B, M - 2, M);
-    bi[0] = 1;
-    for(int i = 1; i < n; ++i) {
-      bi[i] = 1ll * bi[i - 1] * b % M;
+  int inv(int a) {
+    int res = 1;
+    for(int b = M - 2; b; a = 1LL * a * a % M) {
+      if(b & 1) res = 1LL * res * a % M;
+      b >>= 1;
     }
-    h.assign(n, 0);
-    for(int i = 0, p = 1; i < n; ++i, p = 1ll * p * B % M) {
+    return res;
+  }
+public:
+  Hash(const string& s, int base, int mod) : B(base), M(mod) {
+    bi.resize(s.size());
+    h.assign(s.size(), 0);
+    int b = inv(B);
+    bi[0] = 1;
+    for(int i = 1; i < s.size(); ++i) {
+      bi[i] = 1LL * bi[i - 1] * b % M;
+    }
+    for(int i = 0, p = 1; i < s.size(); ++i, p = 1LL * p * B % M) {
       if(i) h[i] = h[i - 1];
-      h[i] += 1ll * (s[i] - 'a') * p % M;
+      h[i] += 1LL * (s[i] - 'a') * p % M;
       if(h[i] >= M) h[i] -= M;
     }
   }
@@ -34,15 +31,14 @@ public:
       ans -= h[i - 1];
       if(ans < 0) ans += M;
     }
-    ans = 1ll * ans * bi[i] % M;
-    return ans;
+    return 1LL * ans * bi[i] % M;
   }
 };
-
+ 
 class DoubleHash {
   Hash h1, h2;
 public:
-  DoubleHash(string s, int b1, int m1, int b2, int m2) :
+  DoubleHash(const string& s, int b1, int m1, int b2, int m2) :
     h1(s, b1, m1), h2(s, b2, m2)
   {
   }
