5d23c49157edb40b59b07e01d0c9180d0fe4faf9
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-04-03 02:56:01 +0430

    Update N_Dimensional_Vector.cc

--- src/tools/N_Dimensional_Vector.cc
+++ src/tools/N_Dimensional_Vector.cc
@@ -1,5 +1,6 @@
 template<int D, typename T>
 struct Vec : public vector<Vec<D - 1, T>> {
+  static_assert(D >= 1, "Vector dimension must be greater than zero!");
   template<typename... Args>
   Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {
 
