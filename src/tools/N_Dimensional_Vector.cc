c1a3c22c3a394d3dea2e32c096fe2890943f2edd
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-04-03 02:49:59 +0430

    Update N_Dimensional_Vector.cc

--- src/tools/N_Dimensional_Vector.cc
+++ src/tools/N_Dimensional_Vector.cc
@@ -1,12 +1,12 @@
 template<int D, typename T>
 struct Vec : public vector<Vec<D - 1, T>> {
   template<typename... Args>
-  Vec(int n, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {
+  Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {
 
   }
 };
 template<typename T>
 struct Vec<1, T> : public vector<T> {
-  Vec(int n, T val = T()) : vector<T>(n, val) {
+  Vec(int n = 0, T val = T()) : vector<T>(n, val) {
   }
 };
