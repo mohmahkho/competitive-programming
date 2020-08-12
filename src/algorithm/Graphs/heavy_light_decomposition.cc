03e3175308242b5fa1d7c36712f82c0ef2c60f9b
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-08-13 04:12:47 +0430

    Update heavy_light_decomposition.cc

--- src/algorithm/Graphs/heavy_light_decomposition.cc
+++ src/algorithm/Graphs/heavy_light_decomposition.cc
@@ -1,10 +1,10 @@
 struct HLD {
   int N, LgN;
-  vector<int> depth, heavy, top, idx;
+  vector<int> depth, heavy, top, pos;
   vector<vector<int>> G, P;
 
   HLD(int n, const vector<pair<int, int>>& edges) : N(n), LgN(32 - __builtin_clz(N)),
-    depth(N), heavy(N, -1), top(N), idx(N), G(N), P(LgN, vector<int>(N)) {
+    depth(N), heavy(N, -1), top(N), pos(N), G(N), P(LgN, vector<int>(N)) {
     for(auto [u, v] : edges) {
       G[u].push_back(v);
       G[v].push_back(u);
@@ -34,8 +34,8 @@ struct HLD {
   }
 
   void decompose(int u, int t) {
-    static int idx_cnt = 0;
-    idx[u] = idx_cnt++;
+    static int pos_cnt = 0;
+    pos[u] = pos_cnt++;
     top[u] = t;
     if(heavy[u] != -1) {
       decompose(heavy[u], t);
@@ -72,7 +72,7 @@ struct HLD {
   void add_path_decomposition(int u, int w, vector<pair<int, int>>& res) {
     while(depth[u] > depth[w]) {
       int t = depth[top[u]] > depth[w] ? top[u] : kth_parent(u, depth[u] - depth[w] - 1);
-      res.emplace_back(idx[t], idx[u]);
+      res.emplace_back(pos[t], pos[u]);
       u = P[0][t];
     }
   }
@@ -81,8 +81,8 @@ struct HLD {
     vector<pair<int, int>> res;
     add_path_decomposition(u, w, res);
     add_path_decomposition(v, w, res);
-    res.emplace_back(idx[w], idx[w]);
+    res.emplace_back(pos[w], pos[w]);
     return res;
-  } // returns a vector of (l, r) where l and r are idxs and segment [l, r] is a part of idxs of path from u to v
+  } // returns a vector of (l, r) where l and r are poss and segment [l, r] is a part of poss of path from u to v
   // vector size is O(LgN)
 };
