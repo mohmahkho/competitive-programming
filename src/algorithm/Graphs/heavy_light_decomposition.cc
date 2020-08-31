210dbe4845ff06478d1a221a7d2e3cb138f81105
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-08-31 21:53:37 +0430

    Update heavy_light_decomposition.cc

--- src/algorithm/Graphs/heavy_light_decomposition.cc
+++ src/algorithm/Graphs/heavy_light_decomposition.cc
@@ -1,28 +1,21 @@
-struct HLD {
-  int N, LgN;
-  vector<int> depth, heavy, top, pos;
-  vector<vector<int>> G, P;
+struct HLD : public LCA {
+  vector<int> heavy;
+  vector<int> top;
+  vector<int> idx;
 
-  HLD(int n, const vector<pair<int, int>>& edges) : N(n), LgN(32 - __builtin_clz(N)),
-    depth(N), heavy(N, -1), top(N), pos(N), G(N), P(LgN, vector<int>(N)) {
-    for(auto [u, v] : edges) {
-      G[u].push_back(v);
-      G[v].push_back(u);
-    }
-    dfs(0, 0);
-    decompose(0, 0);
-    for(int i = 1; i < LgN; ++i) {
-      for(int u = 0; u < N; ++u) {
-        P[i][u] = P[i - 1][P[i - 1][u]];
-      }
-    }
+  HLD(int n_, const vector<pair<int, int>>& edges, int root_ = 0)
+    : LCA(n_, edges, root_) 
+    , heavy(n, -1)
+    , top(n)
+    , idx(n)
+  {
+    dfs(root, -1);
+    decompose(root, root);
   }
 
   int dfs(int u, int p) {
-    P[0][u] = p;
     int u_sz = 1, max_subtree = 0;
-    for(int v : G[u]) if(v != p) {
-      depth[v] = depth[u] + 1;
+    for(int v : g[u]) if(v != p) {
       int v_sz = dfs(v, u);
       u_sz += v_sz;
       if(max_subtree < v_sz) {
@@ -34,55 +27,34 @@ struct HLD {
   }
 
   void decompose(int u, int t) {
-    static int pos_cnt = 0;
-    pos[u] = pos_cnt++;
+    static int idx_cnt = 0;
+    idx[u] = idx_cnt++;
     top[u] = t;
     if(heavy[u] != -1) {
       decompose(heavy[u], t);
     }
-    for(int v : G[u]) if(v != P[0][u] && v != heavy[u]) {
+    for(int v : g[u]) if(v != par[0][u] && v != heavy[u]) {
       decompose(v, v);
     }
-  }
-
-  int lca(int u, int v) {
-    if(depth[u] < depth[v]) {
-      swap(u, v);
-    }
-    for(int msk = depth[u] - depth[v], i = 0; msk; msk >>= 1, ++i) if(msk & 1) {
-      u = P[i][u];
-    }
-    if(u == v) {
-      return u;
-    }
-    for(int i = LgN - 1; i >= 0; --i) {
-      if(P[i][u] != P[i][v]) {
-        u = P[i][u];
-        v = P[i][v];
-      }
-    }
-    return P[0][u];
-  }
-  int kth_parent(int u, int k) {
-    for(int i = 0; k; k >>= 1, ++i) if(k & 1) {
-      u = P[i][u];
-    }
-    return u;
   }
   void add_path_decomposition(int u, int w, vector<pair<int, int>>& res) {
     while(depth[u] > depth[w]) {
       int t = depth[top[u]] > depth[w] ? top[u] : kth_parent(u, depth[u] - depth[w] - 1);
-      res.emplace_back(pos[t], pos[u]);
-      u = P[0][t];
+      res.emplace_back(idx[t], idx[u]);
+      u = par[0][t];
     }
   }
-  vector<pair<int, int>> path_decomposition(int u, int v) {
+
+  /* returns a vector of pairs (l, r) 
+   * where l and r are idxs and segment [l, r] is a part of idxs of path from u to v
+   * returns size is O(lgn)
+   */
+  vector<pair<int, int>> get_path_decomposition(int u, int v) {
     int w = lca(u, v);
     vector<pair<int, int>> res;
     add_path_decomposition(u, w, res);
     add_path_decomposition(v, w, res);
-    res.emplace_back(pos[w], pos[w]);
+    res.emplace_back(idx[w], idx[w]);
     return res;
-  } // returns a vector of (l, r) where l and r are poss and segment [l, r] is a part of poss of path from u to v
-  // vector size is O(LgN)
+  }
 };
