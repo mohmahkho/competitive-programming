a0986bc53584ea6131242b6d6ba4f5400b837ee3
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2020-09-05 02:33:05 +0430

    Create static_variable_in_member_function.cxx

---
+++ mistakes/static_variable_in_member_function.cxx
@@ -0,0 +1,30 @@
+/*
+Consider this example:
+
+struct s {
+  void print() {
+    static int x = 0;
+    x += 1;
+    cout << x << endl;
+  }
+};
+
+-- main --
+s s1;
+s1.print();
+s s2;
+s2.print();
+
+
+Output:
+1
+2
+
+[Probably] Expected output:
+1
+1
+
+This is something that you generally want to avoid.
+Basically you may expect that static variable x reset to 0 for every instance of struct s, but it is not the case.
+Solution is so simple, declare x as a member variable!
+*/
