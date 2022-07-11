71a55745190b5c0c78b6a08a9e8bce0ad9df151e
Mohammad Mahdi Khodabandeh <34912994+mohmahkho@users.noreply.github.com>
2022-07-11 23:22:31 +0430

    Add files via upload

---
+++ src/algorithm/Graphs/bipartite_check.cc
@@ -0,0 +1,21 @@
+auto is_bipartite(const vector<vector<int>>& g) -> bool {
+  int n = (int) g.size();
+  vector<int> color(n, -1);
+  bool bipartite_check = true;
+  auto dfs = [&] (const auto& self, int u, int c) {
+    color[u] = c;
+    for(auto v : g[u]) {
+      if(color[v] == -1) {
+        self(self, v, 1 - c);
+      } else if(color[v] == color[u]) {
+        bipartite_check = false;
+      }
+    }
+  };
+  for(int u = 0; u < n; ++u) {
+    if(color[u] == -1) {
+      dfs(dfs, u, 0);
+    }
+  }
+  return bipartite_check;
+} // O(n)
