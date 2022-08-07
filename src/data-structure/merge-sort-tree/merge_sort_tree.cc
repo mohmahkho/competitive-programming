10e20bfcaae13939ad510928b898aba48e9c543a
mohmahkho <mohmahkho@gmail.com>
2022-08-07 15:27:45 +0430

    Merging merge_sort_tree codes (no pun intended)

--- src/data-structure/merge-sort-tree/merge_sort_tree.cc
+++ src/data-structure/merge-sort-tree/merge_sort_tree.cc
@@ -40,3 +40,27 @@ public:
         return leq(1, from, to, ql, qr, x);
     }
 };
+
+
+/*
+when using merge sort we devide array into two pieces and sort them then merge them up
+so we can create a tree that the first node of it is the whole array but sorted
+left child is first half of the array but sorted and right child is second half of the array but sorted
+
+ for example if we have array 3, 2, 1, 3, 5, 1, 5, 4
+ 
+ then the tree would be like :
+                                1, 1, 2, 3, 3, 4, 5, 5
+                                    /          \
+                                   /            \
+                                  /              \
+                             1, 2, 3, 3      1, 4, 5, 5
+                                ...              ...
+                              
+
+we can use this structure to answer the queries like : "how many numbers are less than 3 in the range of 3, 8"
+in O(lg(n)*lg(n)) using the method that we use to answer queries in segment tree.
+
+the space complexity of this data structure is O(n * lg(n))
+because merge sort tree has lg(n) layers and at each layer we have the whole array. 
+*/
